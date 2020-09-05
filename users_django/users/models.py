77310fa4c302d591176cf28240aeade0eb888070
romain.bouteloup@gmail.com <romain.bouteloup@gmail.com>
2020-09-05 14:29:24 +0200

    max_length requested by CharField for password user attribute

--- users_django/users/models.py
+++ users_django/users/models.py
@@ -8,13 +8,13 @@ class User(models.Model):
         > - all fields but address are mandatory.
         > - address is stored as a single text field for simplicity.
         >If needed in a real production environment, it could be split into several fields or even a dedicated model.
-        > - password is not obfuscated nor encrypted.
+        > - password is not obfuscated nor encrypted. A maximum length is requested by CharField.
     """
 
     first_name = models.CharField(max_length=70, blank=False)
     last_name = models.CharField(max_length=70, blank=False)
     email = models.EmailField(blank=False)
-    password = models.CharField(blank=False)
+    password = models.CharField(max_length=128, blank=False)
     address = models.TextField(max_length=300)
 
 
@@ -23,7 +23,7 @@ class User(models.Model):
 
 
     class Meta:
-        db_tables = 'users'
+        db_table = 'users'
         indexes = [
             models.Index(fields=['last_name', 'first_name'], name='last_first_name_idx'),
             models.Index(fields=['email'], name='email_idx'),
